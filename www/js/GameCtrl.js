fdf95d83d4654202c983482d8a13fbf8f949a030
Katy Justiss <katy.justiss@gmail.com>
2015-09-24 12:23:56 -0500

    added button for game winner

--- www/js/GameCtrl.js
+++ www/js/GameCtrl.js
@@ -13,47 +13,16 @@ angular
 
     GetCards.then(function(res) {
       randomArr.forEach(function(random) {
-        if (res.cards[random]) {
-          return vm.data.push(res.cards[random])
+        if (!res.cards[random]) {
+          random < 32 ? random + 1 : random - 1;
         }
+        return vm.data.push(res.cards[random])
       })
     })
 
-    //vm.getMyTeam = MarvelTeam.getMyTeam()
-    vm.getMyTeam = [
-      { characterid: 2,
-        image: "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/portrait_incredible.jpg",
-        name: "Spider-Man",
-        attack: 6,
-        defense: 5
-      },
-      { characterid: 4,
-        image: "http://i.annihil.us/u/prod/marvel/i/mg/6/40/526963dad214d/portrait_incredible.jpg",
-        name: "Storm",
-        attack: 6,
-        defense: 6
-      },
-      { characterid: 7,
-        image: "http://i.annihil.us/u/prod/marvel/i/mg/3/10/5112d84e2166c/portrait_incredible.jpg",
-        name: "Rogue",
-        attack: 5,
-        defense: 7
-      },
-      { characterid: 21,
-        image: "http://i.annihil.us/u/prod/marvel/i/mg/5/c0/528d340442cca/portrait_incredible.jpg",
-        name: "Juggernaut",
-        attack: 6,
-        defense: 8
-      },
-      { characterid: 30,
-        image: "http://i.annihil.us/u/prod/marvel/i/mg/1/50/526961ce86539/portrait_incredible.jpg",
-        name: "Psylocke",
-        attack: 7,
-        defense: 7
-      }
-    ]
+    vm.getMyTeam = MarvelTeam.getMyTeam()
 
-    //starting game after card click.
+    //START GAME after card click.
     vm.startGame = function (card) {
       vm.isDisabled = true;
       vm.turn++;
@@ -64,51 +33,65 @@ angular
       }, 2000);
     }
 
-    // function Game() {
-    //   playerTurn(card);
-    //   computerTurn(vm.data);
-    //   $timeout(function() {
-    //     calculateScore(getIndex);
-    //   }, 2000);
-    // }
+    //Player Turn
+    function playerTurn(card) {
+      var index = vm.getMyTeam.indexOf(card)
+      moveCard(index)
+      vm.playedCard = card
+      vm.stats = attackDefense(vm.playedCard);
+    }
+
+    //Computer Turn
+    function computerTurn() {
+      var random = randomThree.shift();
+      vm.currentCompCard = vm.data[random]
+      var current = vm.data[random]
+      vm.compStats = attackDefense(vm.currentCompCard);
+      moveCompCard(random);
+    }
 
     function calculateScore(cb) {
       var compStats = vm.compStats;
       var stats = vm.stats;
       var total = compStats + stats;
-      $scope.show = true;
 
       if (compStats/total === stats/total) {
         var random = Math.random();
         if (random > .49) {
           vm.compScore++
-          addAlert({msg: "You almost had 'em"})
+          addAlert({msg: "POW! You almost had 'em"})
         } else {
           vm.score++
-          addAlert({msg: 'Close Call'})
+          addAlert({msg: 'Close call but you managed to pull out the win'})
         }
       } else if(stats/total > compStats/total) {
-        addAlert({msg: 'You win this round'})
+        addAlert({msg: 'Kapow! Round Winner!'})
         vm.score++;
       } else {
         vm.compScore++;
-        addAlert({msg: 'You were outmatched'})
+        addAlert({msg: 'KABOOOM! You were outmatched'})
       }
       $timeout(function() {
         cb(finish);
       }, 2000);
     }
 
+    //DETEMINING WINNER
     function finish() {
       if (vm.score >= 3) {
         vm.turn = 'final';
-        $scope.show = true;
         addAlert({msg: 'CONGRATULATIONS! YOU SAVED THE WORLD'})
+        $('.smplay_btn').css('visibility', 'visible');
       } else if (vm.compScore >= 3) {
         vm.turn = 'final';
-        $scope.show = true;
         addAlert({msg: 'GAME OVER'})
+        $('.smplay_btn').css('visibility', 'visible');
+      } else {
+        vm.alerts = '';
       }
+    }
+    function showButton() {
+
     }
 
     vm.removeCard = {};
@@ -123,6 +106,7 @@ angular
       cb()
     }
 
+    //REMOVE CARDS FROM SCREEN
     function removePlayersCard(index) {
       if(vm.removeCard[index]) {
         vm.removeCard[index] = false;
@@ -141,24 +125,6 @@ angular
       } else {
         vm.removeCompCard[compIndex] = true;
       }
-    }
-
-
-    //Player Turn
-    function playerTurn(card) {
-      var index = vm.getMyTeam.indexOf(card)
-      moveCard(index)
-      vm.playedCard = card
-      vm.stats = attackDefense(vm.playedCard);
-    }
-
-    //Computer Turn
-    function computerTurn() {
-      var random = randomThree.shift();
-      vm.currentCompCard = vm.data[random]
-      var current = vm.data[random]
-      vm.compStats = attackDefense(vm.currentCompCard);
-      moveCompCard(random);
     }
 
     //Card Movement functions
@@ -187,26 +153,19 @@ angular
       var attack = card.attack;
       var defense = card.defense;
       var combined = attack + defense;
-      var min = 0.5;
-      var max = 3.5;
+      var min = 1.0;
+      var max = 5.0;
       combined = Math.round(combined + (1 + Math.random(min, max)));
       return combined;
     };
 
-
+    //ALERT FUNCTIONS
     function addAlert(message) {
       vm.alerts = message;
     }
 
-    vm.closeAlert = function(index) {
-      if (vm.turn !== 'final') {
-        $scope.show = false;
-        vm.alerts = [];
-      }
-    }
 
-
-    //Random numbers utility functions
+    //GENERATING RANDOM NUMBERS
     function randomNum(low, high) {
       var randomArr = [];
       while(randomArr.length < 5) {
@@ -218,10 +177,41 @@ angular
         return randomArr
       }
 
-
     function randomInt (low, high) {
       return Math.floor(Math.random() * (high - low + 1) + low);
     }
 
-
+        // DUMMY DATA
+    // vm.getMyTeam = [
+    //   { characterid: 2,
+    //     image: "http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/portrait_incredible.jpg",
+    //     name: "Spider-Man",
+    //     attack: 6,
+    //     defense: 5
+    //   },
+    //   { characterid: 4,
+    //     image: "http://i.annihil.us/u/prod/marvel/i/mg/6/40/526963dad214d/portrait_incredible.jpg",
+    //     name: "Storm",
+    //     attack: 6,
+    //     defense: 6
+    //   },
+    //   { characterid: 7,
+    //     image: "http://i.annihil.us/u/prod/marvel/i/mg/3/10/5112d84e2166c/portrait_incredible.jpg",
+    //     name: "Rogue",
+    //     attack: 5,
+    //     defense: 7
+    //   },
+    //   { characterid: 21,
+    //     image: "http://i.annihil.us/u/prod/marvel/i/mg/5/c0/528d340442cca/portrait_incredible.jpg",
+    //     name: "Juggernaut",
+    //     attack: 6,
+    //     defense: 8
+    //   },
+    //   { characterid: 30,
+    //     image: "http://i.annihil.us/u/prod/marvel/i/mg/1/50/526961ce86539/portrait_incredible.jpg",
+    //     name: "Psylocke",
+    //     attack: 7,
+    //     defense: 7
+    //   }
+    // ]
   });
